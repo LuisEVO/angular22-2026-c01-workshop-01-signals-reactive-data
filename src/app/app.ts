9a9ef7fe9b899c86ff61044df1372e88783e9141
Luis Vilcarromero <luis.evo_8.10@hotmail.com>
2026-09-22 18:59:31 -0500

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('angular22-2026-c01-workshop-01-signals-reactive-data');
+}
